bc40b63673a4f855e2478a9200ac44f02f8cce9b
Jakob Nordlander <jakob.nordlander@gmail.com>
2019-12-01 11:44:21 +0000

    Added class to read input

---
+++ AdventOfCode2019/CalendarInputTests.cs
@@ -0,0 +1,18 @@
+﻿namespace AdventOfCode2019
+{
+    using Shouldly;
+    using Xunit;
+
+    public class CalendarInputTests
+    {
+        [Fact]
+        public void InputOfIntegers()
+        {
+            var expectedArray = new int[] {113045, 63499, 117820, 67582};
+
+            var result = new CalendarInput("CalendarInputTestInput\\IntegerInput.txt").Read();
+
+            result.ShouldBe(expectedArray);
+        }
+    }
+}
