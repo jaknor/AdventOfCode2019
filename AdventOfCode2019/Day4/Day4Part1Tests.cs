9987c92195fff4831430ba84a51c71f0ed4a67c9
Jakob Nordlander <jakob.nordlander@moonpig.com>
2019-12-04 18:02:37 +0000

    Added second test range validation

--- AdventOfCode2019/Day4/Day4Part1Tests.cs
+++ AdventOfCode2019/Day4/Day4Part1Tests.cs
@@ -15,9 +15,23 @@
         [InlineData(1234567, false)]
         public void Only6DigitPasswordsAreValid(int password, bool shouldBeValid)
         {
-            var fuelDepotPassword = new FuelDepotPassword(password);
+            var fuelDepotPassword = new FuelDepotPasswordValidator(123451, 123459);
 
-            fuelDepotPassword.Valid.ShouldBe(shouldBeValid);
+            fuelDepotPassword.Valid(password).ShouldBe(shouldBeValid);
+        }
+
+        [Theory]
+        [InlineData(111111,222222, 111110, false)]
+        [InlineData(111111,222222, 111111, true)]
+        [InlineData(111111,222222, 111112, true)]
+        [InlineData(111111,222222, 222221, true)]
+        [InlineData(111111,222222, 222222, true)]
+        [InlineData(111111,222222, 222223, false)]
+        public void OnlyPasswordWithinRangeValid(int lowerBound, int upperBound, int password, bool shouldBeValid)
+        {
+            var fuelDepotPasswordValidator = new FuelDepotPasswordValidator(lowerBound, upperBound);
+
+            fuelDepotPasswordValidator.Valid(password).ShouldBe(shouldBeValid);
         }
     }
 }
---
+++ AdventOfCode2019/Day4/FuelDepotPasswordValidator.cs
@@ -0,0 +1,19 @@
+﻿namespace AdventOfCode2019.Day4
+{
+    public class FuelDepotPasswordValidator
+    {
+        private readonly int _lowerBound;
+        private readonly int _upperBound;
+
+        public FuelDepotPasswordValidator(int lowerBound, int upperBound)
+        {
+            _lowerBound = lowerBound;
+            _upperBound = upperBound;
+        }
+
+        public bool Valid(int password)
+        {
+            return password.ToString().Length == 6 && password >= _lowerBound && password <= _upperBound;
+        }
+    }
+}
