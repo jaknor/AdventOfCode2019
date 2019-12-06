08d2792ef8af391f9fbbbf7396b67b7df4b72228
Jakob Nordlander <jakob.nordlander@gmail.com>
2019-12-06 22:10:48 +0000

    Test for example map

--- AdventOfCode2019/Day6/Day6Part1Tests.cs
+++ AdventOfCode2019/Day6/Day6Part1Tests.cs
@@ -128,5 +128,76 @@
 
             result.ShouldBe(8);
         }
+
+        [Fact]
+        public void CanCreateMapForExampleMap()
+        {
+            var result = new OrbitMap(new[]
+            {
+                "COM)B",
+                "B)C",
+                "C)D",
+                "D)E",
+                "E)F",
+                "B)G",
+                "G)H",
+                "D)I",
+                "E)J",
+                "J)K",
+                "K)L"
+            });
+
+            result.Objects.Count.ShouldBe(12);
+            result.Objects.Count(o => o.IsCom).ShouldBe(1);
+            var com = result.Objects.First(o => o.IsCom);
+            com.Children.Count.ShouldBe(1);
+            var B = com.Children[0];
+            var C = B.Children[0];
+            var D = C.Children[0];
+            var E = D.Children[0];
+            var F = E.Children[0];
+            var G = B.Children[1];
+            var H = G.Children[0];
+            var I = D.Children[1];
+            var J = E.Children[1];
+            var K = J.Children[0];
+            var L = K.Children[0];
+
+            B.Children.Count.ShouldBe(2);
+            C.Children.Count.ShouldBe(1);
+            D.Children.Count.ShouldBe(2);
+            E.Children.Count.ShouldBe(2);
+            F.Children.Count.ShouldBe(0);
+            G.Children.Count.ShouldBe(1);
+            H.Children.Count.ShouldBe(0);
+            I.Children.Count.ShouldBe(0);
+            J.Children.Count.ShouldBe(1);
+            K.Children.Count.ShouldBe(1);
+            L.Children.Count.ShouldBe(0);
+        }
+
+        public void CanCalculateOrbitsForExampleMap()
+        {
+            var map = new OrbitMap(new[]
+            {
+                "COM)B",
+                "B)C",
+                "C)D",
+                "D)E",
+                "E)F",
+                "B)G",
+                "G)H",
+                "D)I",
+                "E)J",
+                "J)K",
+                "K)L"
+            });
+
+            var com = map.Objects.First(o => o.IsCom);
+
+            var result = new OrbitCalculator().GetTotalOrbits(com);
+
+            result.ShouldBe(42);
+        }
     }
 }
