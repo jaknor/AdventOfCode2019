e0956d7eebddcca130e9fa255a25c62d8a3a2ff8
Jakob Nordlander <jakob.nordlander@gmail.com>
2019-12-06 22:02:48 +0000

    Test for chain of objects

--- AdventOfCode2019/Day6/Day6Part1Tests.cs
+++ AdventOfCode2019/Day6/Day6Part1Tests.cs
@@ -91,5 +91,42 @@
 
             result.ShouldBe(3);
         }
+
+        [Fact]
+        public void CanCreateMapForComWith1Direct2IndirectAndOneOfThemHasIndirect()
+        {
+            var result = new OrbitMap(new[]
+            {
+                "COM)B",
+                "B)C",
+                "B)E",
+                "E)F",
+            });
+
+            result.Objects.Count.ShouldBe(5);
+            result.Objects.Count(o => o.IsCom).ShouldBe(1);
+            var com = result.Objects.First(o => o.IsCom);
+            com.Children.Count.ShouldBe(1);
+            com.Children[0].Children.Count.ShouldBe(2);
+            com.Children[0].Children[0].Children.Count.ShouldBe(0);
+            com.Children[0].Children[1].Children.Count.ShouldBe(1);
+        }
+
+        public void CanCalculateOrbitsForComWith1Direct2IndirectAndOneOfThemHasIndirect()
+        {
+            var map = new OrbitMap(new[]
+            {
+                "COM)B",
+                "B)C",
+                "B)E",
+                "E)F",
+            });
+
+            var com = map.Objects.First(o => o.IsCom);
+
+            var result = new OrbitCalculator().GetTotalOrbits(com);
+
+            result.ShouldBe(8);
+        }
     }
 }
