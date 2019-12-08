836046ed060142ce59e8dc420f880df1a3fb8ffd
Jakob Nordlander <jakob.nordlander@gmail.com>
2019-12-08 09:15:51 +0000

    First test

---
+++ AdventOfCode2019/Day8/Day8Tests.cs
@@ -0,0 +1,24 @@
+﻿namespace AdventOfCode2019.Day8
+{
+    using Shouldly;
+    using Xunit;
+
+    public class Day8Tests
+    {
+        [Fact]
+        public void OneLayer1x1ImageNoOnesOrTwos()
+        {
+            var verifier = new SpaceImageVerifier();
+
+            verifier.Check("3",1,1).ShouldBe(0);
+        }
+    }
+
+    internal class SpaceImageVerifier
+    {
+        public int Check(string imageDate, int width, int height)
+        {
+            return 0;
+        }
+    }
+}
